efa62c3f2f069c3f230e1e550f590b458852ee9d
JavierLo24 <javierandres.123@hotmail.com>
2024-01-27 17:29:27 -0500

    Initial Commit

---
+++ src/main/java/com/practice/notasbe/NotasBeApplication.java
@@ -0,0 +1,13 @@
+package com.practice.notasbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotasBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotasBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/practice/notasbe/NotasBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.notasbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotasBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
